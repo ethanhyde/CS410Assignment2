27f92c542c6732170eff20f277bba2a8b2ecf35f
Ethan Hyde <ehyde9@gmail.com>
2023-04-19 16:18:29 -0700

    final comments

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/DistanceToEnd.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/DistanceToEnd.cs
@@ -18,13 +18,9 @@ public class DistanceToEnd : MonoBehaviour {
         // Calculate distance value between character and checkpoint
 
         // Found here https://www.youtube.com/watch?v=tg2M35CVP4E
-        // Calculate distance value between character and checkpoint
+       
         distance = (checkpoint.transform.position - transform.position).magnitude;
 
-        // Display distance value via UI text
-        // distance.ToString("F1") shows value with 1 digit after period
-        // so 12.234 will be shown as 12.2 for example
-        // distance.ToString("F2") will show 12.23 in this case
         distanceText.text = "Distance: " + distance.ToString("F1") + " meters";
     }
 
