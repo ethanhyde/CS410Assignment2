0332a995738fabe81e80a67604490b5d329fe41d
Ethan Hyde <ehyde9@gmail.com>
2023-04-19 16:14:53 -0700

    final commit

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/DistanceToEnd.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/DistanceToEnd.cs
@@ -3,19 +3,28 @@ using UnityEngine.UI;
 
 public class DistanceToEnd : MonoBehaviour {
 
-    [SerializeField]
+   [SerializeField]
+
     private Transform checkpoint;
 
     [SerializeField]
     private Text distanceText;
+
     private float distance;
+    
     private void Update()
     {
 
         // Calculate distance value between character and checkpoint
 
         // Found here https://www.youtube.com/watch?v=tg2M35CVP4E
+        // Calculate distance value between character and checkpoint
         distance = (checkpoint.transform.position - transform.position).magnitude;
+
+        // Display distance value via UI text
+        // distance.ToString("F1") shows value with 1 digit after period
+        // so 12.234 will be shown as 12.2 for example
+        // distance.ToString("F2") will show 12.23 in this case
         distanceText.text = "Distance: " + distance.ToString("F1") + " meters";
     }
 
